718bcc84134b88df869eb0acf970ba27fec49da1
Scott Stark <scott.d.stark@gmail.com>
2021-05-17 14:49:39 -0400

    modify app text

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Want To Learn React
+        Learn React
         </a>
       </header>
     </div>
